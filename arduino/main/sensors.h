04fbb4b8645be3e6ffed389d53994d5864960d89
Ryomi <ryomi.j78@gmail.com>
2024-08-18 21:25:49 +0900

    add sensor setting logic

---
+++ arduino/main/sensors.h
@@ -0,0 +1,17 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+
+struct SensorData {
+    float waterLevel;
+    float phValue;
+    float conductivity;
+    float outerTemp;
+    float outerHumidity;
+    float nutrientTemp;
+};
+
+void initializeSensors();
+SensorData readSensors();
+void displayDataOnLCD(float temp, float humidity);
+
+#endif
